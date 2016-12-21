8362eed4df12f81aeaf634b1df29b44dee2f4d46
Archenoth <archenoth@gmail.com>
2016-12-21 03:59:51 -0700

    Considerably better docstrings for the core functions

--- src/clj_anki/core.clj
+++ src/clj_anki/core.clj
@@ -1,5 +1,6 @@
 (ns clj-anki.core
-  "The core library for interacting with Anki files"
+  "The core library for interacting with Anki files. The two main
+  functions of interest are 'read-notes' and 'map-seq-to-package!'"
   (:import java.io.File)
   (require [clj-anki.zip :as zip]
            [clj-anki.record :as rec]
@@ -8,13 +9,28 @@
            [clojure.java.io :as io]))
 
 (def database-spec
-  "The base map to use for database connections."
+  "The a map to use as the base for database connections.
+
+  Set up according to the data source information here:
+  http://clojure-doc.org/articles/ecosystem/java_jdbc/home.html"
   {:classname "org.sqlite.JDBC"
    :subprotocol "sqlite"})
 
 (defn read-notes-from-collection
-  "Reads the :answers, :question, and :tags entries from the notes of
-  an Anki collection."
+  "Given the path to a .anki2 file, reads :answers, :question,
+  and :tags entries from its notes and returns them as a list of maps.
+
+If you had a deck with two cards like:
+
+| Question | Answers | Tags |
+|----------+---------+------|
+| 2 + 2    | 4, Four | math |
+| 3 + 3    | 6, Six  | math |
+
+You would get back a collection of maps like:
+
+({:question \"2 + 2\", :answers [\"4\" \"Four\"], :tags #{\"math\"}}
+ {:question \"3 + 3\", :answers [\"6\" \"Six\"], :tags #{\"math\"}})"
   [anki-collection]
   (map #(assoc % :tags (->> (str/split (:tags %) #" ") (filter (complement str/blank?)) set)
                :answers (rest (str/split (:answers %) #"\x1f")))
@@ -22,9 +38,24 @@
            (sql/query "SELECT sfld AS question, flds AS answers, tags FROM notes"))))
 
 (defn read-notes
-  "Reads notes from an Anki 2 Package and returns them as a list of
-  maps with the :question being the question field, the :answer being
-  a list of answer fields, and the :tags field being a set of tags."
+  "Given the path to a .apkg file, reads :answers, :question,
+  and :tags fields and returns them in the same way
+  read-notes-from-collection does, so given a deck with the cards:
+
+| Question                     | Answers                 | Tags         |
+|------------------------------+-------------------------+--------------|
+| Guy who wrote this sentence  | Archenoth               | people       |
+| Person reading this sentence | (Uh, what's your name?) | people, cool |
+
+You would get back a collection of maps like:
+
+({:question \"Guy who wrote this sentence\",
+  :answers (\"Archenoth\"),
+  :tags #{\"people\"}}
+
+ {:question \"Person reading this sentence\",
+  :answers (\"(Uh, what's your name?)\"),
+  :tags #{\"cool\" \"people\"}})"
   [anki-package]
   (let [tempfile (File/createTempFile "clj-anki" ".sqlite")]
     (zip/extract-file-from-zip! anki-package "collection.anki2" tempfile)
@@ -34,24 +65,64 @@
 
 (defn map-seq-to-collection!
   "Given a sequence of maps with :answers, :tags, and :question
-  entries, this function will create a new Anki collection."
-  [inmap outfile]
+  entries, this function will create a new Anki collection at the path
+  specified by the outfile.
+
+  For each map in the collection:
+  - :question is a String, and will be shown on the front of the card
+  - :answers is a collection of Strings representing the fields on the
+    back of the card
+  - :tags are an optional collection of Strings that will be set as
+    tags. Tags cannot have spaces in them.
+
+  Altogether the first argument for this function should look
+  something like:
+
+  [{:question \"Nice weather we're having!\" :answers [\"Y-you too...\"]}
+   {:question \"What's up?\" :answers [\"I'm good\"]}
+   {:question \"This one or that one?\" :answers [\"Yes\"]}]
+
+  ...give or take some people skills, of course."
+  [inmaps outfile]
   (io/copy (io/input-stream (io/resource "blank.sqlite")) (io/file outfile))
   (sql/with-db-connection [db (assoc database-spec :subname outfile)]
-    (let [values (map rec/convert-map-to-db inmap)]
+    (let [values (map rec/convert-map-to-db inmaps)]
       (sql/insert-multi! db "notes" values)
       (->> (map #(rec/card-record-from-note-id (:id %)) values)
            (sql/insert-multi! db "cards"))))
   outfile)
 
 (defn map-seq-to-package!
-  "Given a sequence of maps with :answers, :tags, and :question
-  entries, this function will create a new Anki package that can be
-  imported into Anki."
-  [inmap outfile]
+  "Like 'map-seq-to-collection!', except the resulting outfile is a
+  full-blown Anki package that can be imported into Anki directly.
+
+  Given a collection of maps with :answers, :tags, and :question
+  entries, this function will create a new Anki package at the path
+  specified by the outfile.
+
+  For each map in the collection:
+
+  - :question is the String that will be shown on the front of the
+    card in the resulting package.
+  - :answers is a collection of Strings representing the fields on the
+    back of the card.
+  - :tags is an optional collection of Strings that will be set as
+    tags. Tags cannot have spaces in them.
+
+  The first argument to this function should look something similar to:
+
+  [{:question \"Are you going to do more useless basic math examples?\"
+    :answers [\"Probably\"]
+    :tags #{\"FAQ\"}}
+   {:question \"Why are you writing different examples for the same structure?\"
+    :answers [\"Because different examples make things easier to remember!\"]
+    :tags #{\"FAQ\"}}
+  {:question \"What does a card with no tags ans two answers look like?\"
+   :answers [\"Like this!\" \"See?\"]}]"
+  [inmaps outfile]
   (let [collection-file (File/createTempFile "clj-anki" ".apkg")
         media-file (File/createTempFile "media" ".json")]
-    (map-seq-to-collection! inmap (.getAbsoluteFile collection-file))
+    (map-seq-to-collection! inmaps (.getAbsoluteFile collection-file))
     (io/copy (io/input-stream (io/resource "media")) media-file)
     (zip/compress-files! [{:name "collection.anki2" :file collection-file}
                           {:name "media" :file media-file}]
