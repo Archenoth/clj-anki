37e3f06443508bca394f90a8ed5ab4f28d6946e7
Archenoth <archenoth@gmail.com>
2017-04-27 10:57:40 -0600

    Updated dependenvies to for clojure.spec
    That way I will be able to conform data, test things, and do all sorts
    of neat stuff that will make clj-anki nicer to use and work on...

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["res"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]])
