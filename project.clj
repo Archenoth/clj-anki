19a589a49b417c5be0c08c3aec36b02fff5f5932
Archenoth <archenoth@gmail.com>
2017-12-29 04:51:05 -0700

    Use the right clojure version and clojure.spec package names

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["res"]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]])
